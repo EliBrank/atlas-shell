9a2b39b6e1b568e6e26f54473f45227585d105d0
EliBrank <eliasbrinkman@gmail.com>
2024-01-12 17:52:49 +0000

    troubleshoot, update fizzbuzz script

--- loops_conditions_and_parsing/example.sh
+++ loops_conditions_and_parsing/example.sh
@@ -3,17 +3,10 @@
 
 for i in {1..100}
 do
-	if [ $(($i%5)) -eq 0 ] && [ $(($i%3)) -eq 0 ]
+	if [ $(( i % 5 == 0 )) ]
 	then
-		echo "FizzBuzz"
-	elif [ $(($i % 3)) -eq 0 ]
-	then
-		echo "Fizz"
-	elif [ $(($i % 3)) -eq 0 ]
-	then
-		echo "Buzz"
+		echo "$((i % 5))"
 	else
 		echo "$i"
 	fi
 done
-
